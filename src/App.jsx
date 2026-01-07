1f01965ab3ab632f9d6deacf0fc68becc32f2ba4
alankritdabral <dabralalankrit@gmai.com>
2026-01-07 19:55:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "https://acelucid-backend.vercel.app//api/recommend",
+        "https://acelucid-backend.vercel.app/api/recommend",
         {
           method: "POST",
           headers: {
